01dc4e8d78fa860282f3d98adc72364082c518e0
UNREALre <avpmanager@gmail.com>
2020-10-11 10:59:23 +0400

    Created UserSource model.

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+"""
+User related models goes here.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from article.views import Source
+
+
+class UserSource(models.Model):
+    """
+    Sources that user select to read
+    """
+
+    user = models.ForeignKey(User, related_name='sources', on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
